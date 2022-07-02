3da3daea45d702bc9c0c4ce4792aa9797abaaac3
CrisRR <curiosity.cdrr@gmail.com>
2022-07-01 20:25:04 -0500

    change/index.js(server up)

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Ok" });
+});
+
+app.post("/", (req, res) => {
+  if (req.query.msg === "OK") {
+    res.status(200).json({ msg: "Ok" });
+  } else {
+    res.status(400).json({ err: "Err" });
+  }
+});
+
+const server = app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 module.exports = {
   sum: (a, b) => a + b,
   res: (a, b) => a - b,
